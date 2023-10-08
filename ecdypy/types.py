adc83a2a5c3581af89703102d9639e53c4b338fc
YammyToast <57455904+YammyToast@users.noreply.github.com>
2023-10-09 00:12:17 +0100

    Primitive Type Interface

--- ecdypy/types.py
+++ ecdypy/types.py
@@ -1,5 +1,21 @@
 """ Abstract Base Class """
-from abc import ABC
+from abc import ABC, abstractmethod
+
+from re import compile
 
 
-class PrimitiveType(ABC):
+class __PrimitiveType(ABC):
+    def __init__(self, __display_form: str, __value_pattern: str) -> None:
+        try:
+            self._display_form = __display_form
+            self._value_pattern = compile(__value_pattern)
+        except Exception as e:
+            print(e)
+
+    @abstractmethod
+    def check_new(self, __value):
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        pass
