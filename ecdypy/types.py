4d729f4d3087194ad34ddb79bf7b34cd0399f185
YammyToast <57455904+YammyToast@users.noreply.github.com>
2023-10-09 01:52:24 +0100

    u8 Primitive Implementation

--- ecdypy/types.py
+++ ecdypy/types.py
@@ -1,21 +1,45 @@
 """ Abstract Base Class """
 from abc import ABC, abstractmethod
 
-from re import compile
+import re
 
 
 class __PrimitiveType(ABC):
-    def __init__(self, __display_form: str, __value_pattern: str) -> None:
+    def __init__(self, __display_form: str) -> None:
         try:
             self._display_form = __display_form
-            self._value_pattern = compile(__value_pattern)
         except Exception as e:
             print(e)
 
     @abstractmethod
-    def check_new(self, __value):
+    def sanitize(self, __value):
+        pass
+
+    @abstractmethod
+    def is_ok(self, __value) -> bool:
         pass
 
     @abstractmethod
     def __str__(self):
         pass
+
+class __U8(__PrimitiveType):
+    _ok_pattern = re.compile(r"^([0-9]|[1-9][0-9]|1[0-9]{2}|(2[0-4][0-9]|25[0-5]))$")
+    _lower_pattern = re.compile(r"^(-[0-9]+)$")
+    _upper_pattern = re.compile(r"^(25[6-9]|2[6-9][0-9]|[3-9][0-9]{2}|[0-9]{4,})$")
+    def sanitize(self, __value):
+        try:
+            if self.is_ok(__value):
+                return __value
+            elif re.search(self._lower_pattern):
+                return 0
+            elif re.search(self._upper_pattern):
+                return 255
+            else:
+                raise
+        except Exception:
+            print(f"Cannot assign value: {__value} to type u8.")
+        
+
+    def is_ok(self, __value: str | int) -> bool:
+        return re.search(self._ok_pattern, str(__value))
